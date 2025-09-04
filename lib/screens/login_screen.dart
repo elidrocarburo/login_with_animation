d1535841c2c0a52109fa08d6c8961391c0611c17
Adriana Cervera Rivadeneyra <princeritsu2209@outlook.com>
2025-09-04 10:17:41 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,14 +9,75 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool passToggle = true;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    //Para obtener el tamaño de la pantalla del dispositivo
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(child: RiveAnimation.asset('assets/animated_login_character.riv'))
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset('assets/animated_login_character.riv')
+                ),
+                //Espacio entre el oso y el texto email
+                const SizedBox(
+                  height: 10,
+                ),
+                //Campo de texto del email
+                TextField(
+                  //qué esperas en ese campo de texto (para que aparezca el @ en móviles)
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    //labelText: "E-mail", para que el hinttext pase arriba del campo de texto
+                    hintText: "E-mail",
+                    prefixIcon: const Icon(Icons.mail),
+                    border: OutlineInputBorder(
+                      //esquinas redondeadas
+                      borderRadius: BorderRadius.circular(12)
+                    )
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                //Campo de texto de contraseña
+                TextField(
+                  //ocultar la contraseña
+                  obscureText: passToggle ? true : false,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    prefixIcon: const Icon(Icons.lock),
+                    suffixIcon: InkWell(
+                      onTap: (){
+                        if(passToggle == true){
+                          passToggle = false;
+                        }
+                        else {
+                          passToggle = true;
+                        }
+                        setState(() {
+                          
+                        });
+                      },
+                      child: passToggle
+                      ? Icon(Icons.remove_red_eye)
+                      : Icon(Icons.visibility_off)
+                      ,
+                    ),
+                    border: OutlineInputBorder(
+                      //esquinas redondeadas
+                      borderRadius: BorderRadius.circular(12)
+                    )
+                  ),
+                ),
+            ],
+          ),
         )),
     );
   }
